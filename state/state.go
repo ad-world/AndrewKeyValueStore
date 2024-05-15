ce74539548a570b4100b1b3a4fd3a1d091bb576b
ad-world <67097683+ad-world@users.noreply.github.com>
2024-05-14 23:32:55 -0400

    Create mechanism to restore state, and add a unit test for it

--- state/state.go
+++ state/state.go
@@ -9,19 +9,41 @@ import (
 
 func SaveState (state *State, akv *akv.AndrewKeyValueStore) error {
 	store := akv.Store
+	// Create a JSON string from the store
 	jsonString, err := json.Marshal(store)
 
 	if err != nil {
 		return err
 	}	
 
+	// Create a file for the string
 	f, err := os.Create(filepath.Join(state.dir, "state.json"))
 	if err != nil {
 		return err
 	}
 	defer f.Close()
 
+	// Write the JSON string to the file
 	f.Write(jsonString)
+
+	return nil
+}
+
+func RestoreState (state *State, akv *akv.AndrewKeyValueStore) error {
+	// Open the state file
+	f, err := os.Open(filepath.Join(state.dir, "state.json"))
+	if err != nil {
+		return err
+	}
+
+	store := akv.Store
+	// Decode the JSON string into the store
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&store)
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
