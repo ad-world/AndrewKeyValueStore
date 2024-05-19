08c55315b5270ca8bbafab8166f876275eb3c561
ad-world <67097683+ad-world@users.noreply.github.com>
2024-05-19 15:45:29 -0400

    Create basic cli program to interact with server

--- akv_cli/akv_cli.go
+++ akv_cli/akv_cli.go
@@ -6,7 +6,15 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"akv/akv"
+	"net/rpc"
 )
+
+func printCommandUsage() {
+	fmt.Println("Invalid command: ")
+	fmt.Println("Usage: <command> <key> <value>")
+	fmt.Println("Commands: get, put, delete")
+}
 
 func main() {
 	var address string 
@@ -21,6 +29,14 @@ func main() {
 
 	in := bufio.NewReader(os.Stdin)
 
+	andrew := new(akv.AndrewKeyValueClient)
+	var err error
+	andrew.Client, err = rpc.Dial("tcp", address)
+
+	if err != nil {
+		fmt.Println("Error in connecting to server:", err)
+	} 
+
 	for {
 		fmt.Print("akv_cli> ")
 		var command string
@@ -28,8 +44,50 @@ func main() {
 		stripped := strings.TrimSpace(command)
 		if stripped == "exit" {
 			break
-		} else {
-			fmt.Println(command)
+		}
+
+		parts := strings.Split(stripped, " ")
+		if len(parts) < 1 {
+			printCommandUsage()
+			continue
+		}
+
+		switch parts[0] {
+		case "get":
+			if len(parts) != 2 {
+				printCommandUsage()
+				continue
+			}
+			value, err := andrew.Get(parts[1])
+			if err != nil {
+				fmt.Println("Error in Get:", err)
+				continue
+			}
+			fmt.Println("Value of", parts[1], "is", value)
+		case "put":
+			if len(parts) != 3 {
+				printCommandUsage()
+				continue
+			}
+			_, err := andrew.Put(parts[1], parts[2])
+			if err != nil {
+				fmt.Println("Error in Put:", err)
+				continue
+			}
+			fmt.Println("Put successful")
+		case "delete":
+			if len(parts) != 2 {
+				printCommandUsage()
+				continue
+			}
+			_, err := andrew.Delete(parts[1])
+			if err != nil {
+				fmt.Println("Error in Delete:", err)
+				continue
+			}
+			fmt.Println("Delete successful")
+		default:
+			printCommandUsage()
 		}
 	}
 }
